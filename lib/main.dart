968ff2ca58e6f80772bd630e47be7543b03427fb
ayush <mehtaayush20000@gmail.com>
2024-10-29 19:21:45 +0530

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(myapp())
+}
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
